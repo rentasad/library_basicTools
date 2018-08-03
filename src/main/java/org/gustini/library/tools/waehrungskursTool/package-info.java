fa4b12ee799c82816aa1ae8fa9dc54dbb5f1e915
Matthias Staud <matthias.staud@gustini.de>
2018-08-03 11:23:28 +0200

    Add WährungskursTool

---
+++ src/main/java/org/gustini/library/tools/waehrungskursTool/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * Gustini GmbH (2015)
+ * Creation: 15.09.2015
+ * Rentasad Library
+ * rentasad.lib.tools.waehrungskursTool
+ * 
+ * @author Matthias Staud
+ *
+ *
+ *         Description:
+ * 	Ermittelt anhand einer API den Tagesaktuellen Waehrungskurs und pflegt ihn in die Datenbank ein.
+ */
+package org.gustini.library.tools.waehrungskursTool;
