1b45f33c02321bcee00ba5c3d3fe05a3b5763ecd
Matthias Staud <matthias.staud@gustini.de>
2018-09-03 11:40:34 +0200

    Create Emtpy Class

---
+++ src/main/java/org/gustini/library/tools/heartBeatMonitoring/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * Gustini GmbH (2018)
+ * Creation: 03.09.2018
+ * gustini.library.basicTools
+ * org.gustini.library.tools.heartBeatMonitoring
+ * 
+ * @author Matthias Staud
+ *
+ *
+ * Description:Enthält Toolkit für "HeartBeatMonitoring"
+ *
+ */
+package org.gustini.library.tools.heartBeatMonitoring;
