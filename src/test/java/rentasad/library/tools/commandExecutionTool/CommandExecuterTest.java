c0ef1b253a08f8af912bf925ddf03ea0734f6006
Matthias Staud <matthias.staud@gustini.de>
2020-11-26 16:57:37 +0100

    CommadnExecuterTest added

---
+++ src/test/java/rentasad/library/tools/commandExecutionTool/CommandExecuterTest.java
@@ -0,0 +1,27 @@
+package rentasad.library.tools.commandExecutionTool;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class CommandExecuterTest
+{
+
+	@BeforeAll
+	public void setUp() throws Exception
+	{
+	}
+
+	@AfterAll
+	public void tearDown() throws Exception
+	{
+	}
+
+	@org.junit.jupiter.api.Test
+	public void testConnect() throws Exception
+	{
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
