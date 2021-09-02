da8d6106c7a3d5504defc881b3630738d1f487dd
matthias.staud@gustini.de <matthias.staud@gustini.de>
2021-09-02 20:15:16 +0200

    add FileOperator

---
+++ rentasad.library.basicTools.fileOperator/src/main/java/rentasad/library/tools/fileOperator/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * Gustini GmbH (2016)
+ * Creation: 15.12.2016
+ * Library
+ * rentasad.library.tools.fileOperator
+ * 
+ * @author Matthias Staud
+ *
+ *
+ * Description:
+ *
+ */
+package rentasad.library.tools.fileOperator;
